05d9da1bf2aa49e3ebbe175ecd8bb5ce3f9bb4b2
Simon Wilson <contact@simonwilson.design>
2023-03-06 20:28:15 +0000

    Added favicon

--- ifla-web/app/components/parts/CartPreviewItem.jsx
+++ ifla-web/app/components/parts/CartPreviewItem.jsx
@@ -2,13 +2,13 @@ import React, {useRef} from 'react';
 
 export const CartPreviewItem = ({item}) => {
   const rotation = useRef(Math.floor(Math.random() * 50) - 25);
-  const positionX = useRef(Math.floor(Math.random() * 75) + 1);
-  const positionY = useRef(Math.floor(Math.random() * 50) + 1);
+  const positionX = useRef(Math.floor(Math.random() * 60) + 1);
+  const positionY = useRef(Math.floor(Math.random() * 30) + 1);
   return (
     <img
       role="presentation"
       src={item.merchandise.image.url}
-      className={'absolute h-1/2'}
+      className={'absolute h-3/4'}
       style={{
         transform: `rotate(${rotation.current}deg)`,
         left: `${positionX.current}%`,
