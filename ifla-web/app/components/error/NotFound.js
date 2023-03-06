06d21f34a55d3c66f495c339a41c1a4599c06af0
Simon Wilson <contact@simonwilson.design>
2023-03-06 19:30:39 +0000

    Added addLinksToStackTrace

--- ifla-web/app/components/error/NotFound.js
+++ ifla-web/app/components/error/NotFound.js
@@ -23,3 +23,14 @@ export function NotFound({error}) {
     </Layout>
   );
 }
+
+function addLinksToStackTrace(stackTrace) {
+  return stackTrace?.replace(
+    /^\s*at\s?.*?[(\s]((\/|\w\:).+)\)\n/gim,
+    (all, m1) =>
+      all.replace(
+        m1,
+        `<a href="vscode://file${m1}" class="hover:underline">${m1}</a>`,
+      ),
+  );
+}
