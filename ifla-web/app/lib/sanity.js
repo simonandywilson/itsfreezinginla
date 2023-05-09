f326b2dda06caff6573d107ed062f7be7b845d6e
Simon Wilson <contact@simonwilson.design>
2023-05-09 17:39:28 +0100

    Updated lib.sanity to use production dataset

--- ifla-web/app/lib/sanity.js
+++ ifla-web/app/lib/sanity.js
@@ -1,7 +1,7 @@
 import {createClient} from '@sanity/client';
 
 export const projectId = 'yt08sdph';
-export const dataset = 'staging';
+export const dataset = 'production';
 export const apiVersion = '2023-01-01';
 
 export const sanityClient = createClient({projectId, dataset, apiVersion, useCdn: true});
