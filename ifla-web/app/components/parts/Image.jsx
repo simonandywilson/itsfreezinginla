daaaa7a10c9625eda0dde2136087692860440e42
Simon Wilson <contact@simonwilson.design>
2023-05-22 15:31:40 +0100

    Reverted image changes

--- ifla-web/app/components/parts/Image.jsx
+++ ifla-web/app/components/parts/Image.jsx
@@ -1,10 +1,13 @@
-import {useRouteLoaderData} from '@remix-run/react';
 import {SanityImage} from 'sanity-image';
 
+const projectId = 'yt08sdph';
+// const dataset =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+//     ? 'staging'
+//     : 'production';
+const dataset = 'production';
+const baseUrl = `https://cdn.sanity.io/images/${projectId}/${dataset}/`;
+
 export const Image = (props) => {
-  const { ENV } = useRouteLoaderData('root');
-  const projectId = ENV.SANITY_PUBLIC_PROJECT_ID;
-  const dataset = ENV.SANITY_PUBLIC_DATASET;
-  const baseUrl = `https://cdn.sanity.io/images/${projectId}/${dataset}/`;
   return <SanityImage baseUrl={baseUrl} {...props} />;
 };
